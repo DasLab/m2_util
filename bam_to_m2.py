f8e0a890be07cb4e86929cab3351cdeedab7427d
Rhiju Das <rhiju@stanford.edu>
2025-06-06 13:57:34 -0700

    start_idx and  end_idx, with help from Claude 3.7, though wasted lots of time with bugs introduced by LLM!

--- bam_to_m2.py
+++ bam_to_m2.py
@@ -16,12 +16,14 @@ parser.add_argument('-mq','--map_quality',default=10,type=int,help='minimum Bowt
 parser.add_argument('--mutdel_cutoff',type=int,default=10,help='Filter for maximum number of mut/del in read (default 0 means no filter)' )
 parser.add_argument('-o','--out_tag','--out_file_tag',type=str,default='',help='Tag for outfile [default: derive from first bamfile]' )
 parser.add_argument('-n','--chunk_size', default=0, type=int, help='split with this number of sequences per chunk')
+parser.add_argument('--start_idx', default=0, type=int, help='only do the reference sequences from start_idx onwards [default all]')
+parser.add_argument('--end_idx', default=0, type=int, help='only do the reference sequences up to end_idx [default all]')
 
 assert(shutil.which('samtools') )
 args = parser.parse_args()
 
-def read_fasta( fasta_file ):
-    lines = open( fasta_file ).readlines()
+def read_fasta(fasta_file):
+    lines = open(fasta_file).readlines()
     sequences = []
     headers = []
     header = None
@@ -38,10 +40,10 @@ def read_fasta( fasta_file ):
     if header is not None:
         headers.append(header)
         sequences.append(sequence)
-    assert( len(sequences) == len(headers ) )
-    return (sequences,headers)
+    assert(len(sequences) == len(headers))
+    return (sequences, headers)
 
-(ref_sequences,ref_headers) = read_fasta( args.sequences_fasta )
+(ref_sequences, ref_headers) = read_fasta(args.sequences_fasta)
 ref_headers = [header.split()[0] for header in ref_headers]
 ref_sequences = [sequence.replace('U','T') for sequence in ref_sequences] # need to convert to DNA!
 Nref = len(ref_headers)
@@ -49,135 +51,144 @@ Nref = len(ref_headers)
 if len(args.bam)>1 and len(args.out_tag)>1:
     print('With multiple bam files, cannot supply --out_tag. Rerun without --out_tag and output files will have tags based on .bam files')
 
-def initialize_counts_2d( counts_2d, coverage, Nres ):
-    for i in ['mut','del']:
-        if i not in counts_2d: counts_2d[i] = {}
-        for j in ['mut','del']:
-            counts_2d[i][j] = []
-            for n in range(Nres):
-                counts_2d[i][j].append( [0]*Nres )
-    if len(coverage)==0:
-        for n in range(Nres): coverage.append(0)
-    for n in range(Nres): coverage[n] = 0
+chunk_size = args.chunk_size
+start_idx = args.start_idx
+end_idx = args.end_idx
+assert(not(chunk_size>0 and start_idx>0))
 
-def update_out_files( fids, out_tag,ref_idx, chunk_size, Nref, gzipped=True ):
-    if gzipped:
-        file_open = gzip.open
-        gzip_ext=".gz"
-    else:
-        file_open=open
-        gzip_ext=""
-    if len(fids)>0:
-        for (fid_2d,fid_1d,fid_coverage) in fids[-1]:
-            for tag1 in ['mut','del']:
-                fid_1d[tag1].close()
-                for tag2 in ['mut','del']:
-                    fid_2d[tag1][tag2].close()
-            fid_2d['mutdel'].close()
-            fid_1d['mutdel'].close()
-            fid_coverage.close()
+# Set end_idx to Nref if not specified
+if end_idx == 0:
+    end_idx = Nref
 
-    #TODO actually use split_tag
-    split_tag=''
-    if chunk_size>0 and Nref>chunk_size:
-        chunk_start = ref_idx+1
-        chunk_end   = min( ref_idx + chunk_size, len(ref_headers))
-        split_tag='.%07d_%07d' % (chunk_start,chunk_end)
-
-    if ref_idx < Nref+1 or chunk_size==0:
-        fid_2d = {}
-        fid_1d = {}
-        for tag1 in ['mut','del']:
-            fid_2d[tag1] = {}
-            for tag2 in ['mut','del']:
-                outfile = out_tag + '.counts_%s_%s.txt' % (tag1,tag2)
-                fid_2d[tag1][tag2] = file_open( outfile+gzip_ext, 'wt')
-            outfile = out_tag + '.counts_%s.txt' % (tag1)
-            fid_1d[tag1] = file_open( outfile+gzip_ext, 'wt' )
-        outfile = out_tag + '.counts_%s_%s.txt' % ('mutdel','mutdel')
-        fid_2d['mutdel'] = file_open( outfile+gzip_ext, 'wt' )
-        outfile = out_tag + '.counts_%s.txt' % ('mutdel')
-        fid_1d['mutdel'] = file_open( outfile+gzip_ext, 'wt' )
-        outfile = out_tag + '.coverage.txt'
-        fid_1d['coverage'] = file_open( outfile+gzip_ext, 'wt' )
-        fids.append( (fid_2d,fid_1d) )
-
-def output_to_index( ref_idx, counts_2d, coverage, fids ):
-    (fid_2d, fid_1d) = fids[-1]
-    Nres = len(counts_2d['mut']['mut'])
-    # 2D profiles
+def initialize_counts_2d(counts_2d, coverage, Nres):
     for tag1 in ['mut','del']:
         for tag2 in ['mut','del']:
-            for n1 in range(Nres):
-                fid_2d[tag1][tag2].write(",".join(map(str, counts_2d[tag1][tag2][n1])) + "\n")
+            tag = tag1 + '_' + tag2
+            counts_2d[tag] = []
+            for n1 in range(Nres): counts_2d[tag].append([0]*Nres)
+    for n in range(Nres): coverage.append(0)
 
-    # 1D profiles
-    counts_1d={}
+def output_to_index(ref_idx, counts_2d, coverage, fids):
+    if ref_idx >= Nref or ref_idx >= end_idx:
+        return (ref_idx, None)
+
+    Nres = len(ref_sequences[ref_idx])
+    fid_1d = fids[0]
+    fid_2d = fids[1]
+
+    counts_1d = {}
     for tag in ['mut','del']:
-        counts_1d[tag] = [counts_2d[tag][tag][n][n] for n in range(Nres)]
-        fid_1d[tag].write( ",".join(map(str,counts_1d[tag]))+"\n")
-    fid_1d['coverage'].write( ",".join(map(str,coverage))+"\n")
+        counts_1d[tag] = [counts_2d[tag+'_'+tag][n][n] for n in range(Nres)]
+        fid_out = fid_1d[tag]
+        fid_out.write(",".join(map(str, counts_1d[tag])) + "\n")
 
-    # combine mut/del
+    for tag1 in ['mut','del']:
+        for tag2 in ['mut','del']:
+            tag = tag1 + '_' + tag2
+            fid_out = fid_2d[tag]
+            for n1 in range(Nres):
+                fid_out.write(",".join(map(str, counts_2d[tag][n1])) + "\n")
+
+    fid_out = fid_1d['coverage']
+    fid_out.write(",".join(map(str, coverage)) + "\n")
+
     counts_2d_all = []
-    for n in range( Nres ): counts_2d_all.append( [0]*Nres )
+    for n in range(Nres): counts_2d_all.append([0]*Nres)
     fid_out = fid_2d['mutdel']
     for n1 in range(Nres):
         for n2 in range(Nres):
             for tag1 in ['mut','del']:
                 for tag2 in ['mut','del']:
-                    counts_2d_all[n1][n2] += counts_2d[tag1][tag2][n1][n2]
+                    counts_2d_all[n1][n2] += counts_2d[tag1+'_'+tag2][n1][n2]
         fid_out.write(",".join(map(str, counts_2d_all[n1])) + "\n")
 
-    counts_1d['mutdel'] = [ counts_1d['mut'][n]+counts_1d['del'][n] for n in range(Nres)]
+    counts_1d['mutdel'] = [counts_1d['mut'][n]+counts_1d['del'][n] for n in range(Nres)]
     fid_out = fid_1d['mutdel']
     fid_out.write(",".join(map(str, counts_1d['mutdel'])) + "\n")
 
     ref_idx += 1
-    initialize_counts_2d(counts_2d, coverage, Nres)
-    if chunk_size>0 and (ref_idx+1) % chunk_size == 1:
-        update_out_files( fids, out_tag,ref_idx, chunk_size, Nref)
+    if ref_idx < end_idx:
+        initialize_counts_2d(counts_2d, coverage, len(ref_sequences[ref_idx]))
 
     ref_sequence = None
-    if ref_idx < len(ref_sequences): ref_sequence = ref_sequences[ref_idx]
+    if ref_idx < len(ref_sequences) and ref_idx < end_idx:
+        ref_sequence = ref_sequences[ref_idx]
     return (ref_idx, ref_sequence)
 
-chunk_size = args.chunk_size
-for bam in args.bam:
+def update_out_files(fids, out_tag, ref_idx, chunk_size, Nref):
+    if len(fids) > 0:
+        for fid_set in fids:
+            for fid in fid_set.values():
+                fid.close()
+        fids.clear()
 
-    print( 'Doing BAM file %s' % bam )
-    assert( bam[-4:]=='.bam' or  bam[-5:]=='.cram' )
+    num_digits = len(str(Nref))
+    suffix = f'.{start_idx:0{num_digits}d}_{end_idx:0{num_digits}d}'
+
+    fid_1d = {}
+    fid_2d = {}
+    for tag in ['mut', 'del', 'mutdel']:
+        fid_1d[tag] = gzip.open(f'{out_tag}{suffix}.counts_{tag}.txt.gz', 'wt')
+
+    # Separate naming for coverage file
+    fid_1d['coverage'] = gzip.open(f'{out_tag}{suffix}.coverage.txt.gz', 'wt')
+
+    for tag1 in ['mut', 'del']:
+        for tag2 in ['mut', 'del']:
+            tag = f'{tag1}_{tag2}'
+            fid_2d[tag] = gzip.open(f'{out_tag}{suffix}.counts_{tag}.txt.gz', 'wt')
+
+    fid_2d['mutdel'] = gzip.open(f'{out_tag}{suffix}.counts_mutdel.txt.gz', 'wt')
+
+    fids.append(fid_1d)
+    fids.append(fid_2d)
+
+# Create regions file for samtools
+regions_file = f'{args.sequences_fasta}.{start_idx:07d}_{end_idx:07d}.bed'
+with open(regions_file, 'w') as fid_regions:
+    for i in range(start_idx-1, end_idx):
+        fid_regions.write(f'{ref_headers[i]} 0 {len(ref_sequences[i])}\n')
+regions_tag = f' --regions-file {regions_file}'
+
+for bam in args.bam:
+    print(f'Doing BAM file {bam}')
+    assert(bam.endswith('.bam') or bam.endswith('.cram'))
 
     out_tag = args.out_tag
     if len(out_tag)==0: out_tag = bam.replace('.bam','').replace('.cram','')
 
-    command = "samtools view %s | awk '$5>=%d'" % (bam,args.map_quality )
-    fid_bam = os.popen( command )
+    command = f"samtools view {bam} {regions_tag} | awk '$5>={args.map_quality}'"
+    fid_bam = os.popen(command)
 
     line = fid_bam.readline()
     cols = line.split()
-    ref_idx = 0 # where we are in reference sequence file, 0 indexed
+    ref_idx = start_idx - 1
     header = ''
 
     count = 0
     count_filter = 0
 
-    fids = [] # outfiles
+    fids = []
 
     header = line.split()[2]
-    ref_sequence = ref_sequences[ ref_headers.index( header ) ]
+    ref_sequence = ref_sequences[ref_headers.index(header)]
     Nres = len(ref_sequence)
-    update_out_files( fids, out_tag,ref_idx, chunk_size, Nref)
+    update_out_files(fids, out_tag, ref_idx, chunk_size, Nref)
     counts_2d = {}
     coverage = []
-    initialize_counts_2d( counts_2d, coverage, Nres )
+    initialize_counts_2d(counts_2d, coverage, Nres)
 
     while line:
         cols = line.split()
         next_header = cols[2]
-        if header != next_header: # we're ready to output the 2D stats
+        if header != next_header:
             while ref_headers[ref_idx] != next_header:
-                ref_idx, ref_sequence = output_to_index( ref_idx, counts_2d, coverage, fids )
+                ref_idx, ref_sequence = output_to_index(ref_idx, counts_2d, coverage, fids)
+                if ref_sequence is None:
+                    break
+            header = next_header
+            if ref_sequence is None:
+                break
 
         start_pos = int(cols[3])
         cigar = cols[5]
@@ -185,8 +196,8 @@ for bam in args.bam:
         read = cols[9]
 
         seqa = ''
-        read_pos = 0  # position in read
-        seqa += '.' * (start_pos - 1)  # pad start
+        read_pos = 0
+        seqa += '.' * (start_pos - 1)
 
         # Parse CIGAR with regex: [('22', 'M'), ('1', 'I'), ...]
         cigar_tuples = re.findall(r'(\d+)([MIDNSHP=X])', cigar)
@@ -211,25 +222,24 @@ for bam in args.bam:
         assert(len(seqa) <= len(ref_sequence))
         end_pos = len(seqa)
 
-        # Ensure full alignment length
         if signed_tmpl_len < 0:
             seqa = '.' * (len(ref_sequence) - len(seqa)) + seqa
         else:
             seqa = seqa + '.' * (len(ref_sequence) - len(seqa))
 
-        assert( len(seqa) == len(ref_sequence) )
+        assert len(seqa) == len(ref_sequence)
 
-        pos = {}
-        pos['mut'] = []
-        pos['del'] = []
-        for n,nt_pair in enumerate(zip(ref_sequence,seqa)):
-            if nt_pair[0] not in 'ACGT-': continue
-            if nt_pair[1] not in 'ACGT-': continue
-            if nt_pair[1] == '-': pos['del'].append(n)
-            elif nt_pair[0] != nt_pair[1]: pos['mut'].append(n)
+        pos = {'mut': [], 'del': []}
+        for n, (ref_nt, read_nt) in enumerate(zip(ref_sequence, seqa)):
+            if ref_nt not in 'ACGT-': continue
+            if read_nt not in 'ACGT-': continue
+            if read_nt == '-':
+                pos['del'].append(n)
+            elif ref_nt != read_nt:
+                pos['mut'].append(n)
 
         count += 1
-        if count % 1000 == 0: print('Processed %10d lines...' % count, '  and number that pass filter: %10d' % count_filter)
+        if count % 1000 == 0: print(f'Processed {count:10d} lines...  and number that pass filter: {count_filter:10d}')
 
         total_mutdel = len(pos['mut']) + len(pos['del'])
         if args.mutdel_cutoff == 0 or total_mutdel <= args.mutdel_cutoff:
@@ -238,19 +248,21 @@ for bam in args.bam:
                 for tag2 in ['mut','del']:
                     for n1 in pos[tag1]:
                         for n2 in pos[tag2]:
-                            counts_2d[tag1][tag2][n1][n2] += 1
-            for n in range(start_pos-1,end_pos): coverage[n] += 1
+                            counts_2d[tag1+'_'+tag2][n1][n2] += 1
+            for n in range(start_pos-1, end_pos):
+                coverage[n] += 1
 
         line = fid_bam.readline()
 
-    while ref_idx<Nref:
-        ref_idx, ref_sequence = output_to_index( ref_idx, counts_2d, coverage, fids )
+    while ref_idx < end_idx:
+        ref_idx, ref_sequence = output_to_index(ref_idx, counts_2d, coverage, fids)
 
-    fid_bam.close()
-    print('Processed %d from %d lines\n' % (count_filter,count) )
-    print('Created %d sets of files with names like:' % len(fids) )
-    (fid_2d, fid_1d) = fids[0]
-    for tag1 in ['mut','del']:
-        for tag2 in ['mut','del']:
-            print(fid_2d[tag1][tag2].name)
-    for tag in ['mut','del','mutdel','coverage']: print( fid_1d[tag].name)
+    for fid_set in fids:
+        for fid in fid_set.values():
+            fid.close()
+
+    print(f'Processed {count} reads. Filtered {count_filter} reads with <= {args.mutdel_cutoff} mutations+deletions.')
+
+os.remove(regions_file)
+
+
